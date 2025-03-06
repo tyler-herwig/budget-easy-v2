185139f26327a1b5aade0827997e5c0ee8190e47
= <tylerherwig@gmail.com>
2025-03-06 11:50:32 -0500

    Fixing income column on scroll, and added loading spinner.

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useMemo, useState } from 'react';
-import { Grid, Container, Card, CardContent, Typography, Table, TableHead, TableBody, TableRow, TableCell, Box, Button, Alert, AlertTitle } from '@mui/material';
+import { Grid, Container, Card, CardContent, Typography, Table, TableHead, TableBody, TableRow, TableCell, Box, Alert, AlertTitle } from '@mui/material';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -13,6 +13,7 @@ import { Income } from '@/types/income';
 import { fetchExpenses } from '@/utils/fetch/expense';
 import { groupByMonthYear } from '@/utils/helpers/expense';
 import { fetchIncome } from '@/utils/fetch/income';
+import { LoadingSpinner } from './LoadingSpinner';
 
 const Dashboard: React.FC = () => {
   const [startDate, setStartDate] = useState<Moment | null>(null);
@@ -45,8 +46,22 @@ const Dashboard: React.FC = () => {
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
-        <Container maxWidth="xl" sx={{ mt: 3, mb: 3 }}>
-            <Box sx={{ display: 'flex', mb: 3, gap: 2 }}>
+        <Box
+          sx={{
+            position: 'sticky', 
+            top: 68.5, 
+            backgroundColor: 'white',
+            zIndex: 1,
+            boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)'
+          }}
+        >
+          <Container maxWidth="xl" sx={{ mb: 3, pt: 2, pb: 2 }}>
+          <Box 
+              sx={{ 
+                display: 'flex', 
+                gap: 2
+              }}
+            >
                 <DatePicker 
                     label="Start Date" 
                     value={startDate}
@@ -58,6 +73,9 @@ const Dashboard: React.FC = () => {
                     onChange={handleEndDateChange}
                 />
             </Box>
+          </Container>
+        </Box>
+        <Container maxWidth="xl" sx={{ mt: 3, mb: 3 }}>
             {(!incomes || !expenses) && (!startDate || !endDate) ? (
               <Alert variant="outlined" severity="info">
                 <AlertTitle>Get Started</AlertTitle>
@@ -66,66 +84,74 @@ const Dashboard: React.FC = () => {
             ) : (
               <Grid container spacing={3}>
                 <Grid item xs={12} md={5}>
-                    <Card>
-                        <CardContent>
-                          <Typography variant="h6" gutterBottom>
-                              Income
-                          </Typography>
-                          {isLoadingIncome ? (
-                            <Typography>Loading income...</Typography>
-                          ) : isErrorIncome ? (
-                            <Typography color="error">{(incomeError as Error).message || 'Failed to fetch income'}</Typography>
-                          ) : (
-                            <Table>
-                              <TableHead>
-                                  <TableRow>
-                                    <TableCell>Income Date</TableCell>
-                                    <TableCell>Amount</TableCell>
-                                    <TableCell>Total Expenses</TableCell>
-                                    <TableCell>Money Remaining</TableCell>
-                                  </TableRow>
-                              </TableHead>
-                              <TableBody>
-                                {incomes?.map((income) => (
-                                  <TableRow key={income.id}>
-                                    <TableCell>{moment(income.date_received).format('MMMM Do, YYYY')}</TableCell>
-                                    <TableCell>
-                                      <NumericFormat
-                                          value={income.amount || 0}
-                                          displayType="text"
-                                          thousandSeparator={true}
-                                          prefix="$"
-                                          decimalScale={2}
-                                          fixedDecimalScale={true}
-                                      />
-                                    </TableCell>
-                                    <TableCell>
-                                      <NumericFormat
-                                          value={income.total_expenses || 0}
-                                          displayType="text"
-                                          thousandSeparator={true}
-                                          prefix="$"
-                                          decimalScale={2}
-                                          fixedDecimalScale={true}
-                                      />
-                                    </TableCell>
-                                    <TableCell>
-                                      <NumericFormat
-                                          value={income.money_remaining || 0}
-                                          displayType="text"
-                                          thousandSeparator={true}
-                                          prefix="$"
-                                          decimalScale={2}
-                                          fixedDecimalScale={true}
-                                      />
-                                    </TableCell>
-                                  </TableRow>
-                                ))}
-                              </TableBody>
-                            </Table>
-                          )}
-                        </CardContent>
-                    </Card>
+                    <Box 
+                      sx={{ 
+                        position: 'sticky', 
+                        top: 180, 
+                        backgroundColor: 'white' 
+                      }}
+                    >
+                      <Card>
+                          <CardContent>
+                            <Typography variant="h6" gutterBottom>
+                                Income
+                            </Typography>
+                            {isLoadingIncome ? (
+                              <LoadingSpinner />
+                            ) : isErrorIncome ? (
+                              <Typography color="error">{(incomeError as Error).message || 'Failed to fetch income'}</Typography>
+                            ) : (
+                              <Table>
+                                <TableHead>
+                                    <TableRow>
+                                      <TableCell>Income Date</TableCell>
+                                      <TableCell>Amount</TableCell>
+                                      <TableCell>Total Expenses</TableCell>
+                                      <TableCell>Money Remaining</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                  {incomes?.map((income) => (
+                                    <TableRow key={income.id}>
+                                      <TableCell>{moment(income.date_received).format('MMMM Do, YYYY')}</TableCell>
+                                      <TableCell>
+                                        <NumericFormat
+                                            value={income.amount || 0}
+                                            displayType="text"
+                                            thousandSeparator={true}
+                                            prefix="$"
+                                            decimalScale={2}
+                                            fixedDecimalScale={true}
+                                        />
+                                      </TableCell>
+                                      <TableCell>
+                                        <NumericFormat
+                                            value={income.total_expenses || 0}
+                                            displayType="text"
+                                            thousandSeparator={true}
+                                            prefix="$"
+                                            decimalScale={2}
+                                            fixedDecimalScale={true}
+                                        />
+                                      </TableCell>
+                                      <TableCell>
+                                        <NumericFormat
+                                            value={income.money_remaining || 0}
+                                            displayType="text"
+                                            thousandSeparator={true}
+                                            prefix="$"
+                                            decimalScale={2}
+                                            fixedDecimalScale={true}
+                                        />
+                                      </TableCell>
+                                    </TableRow>
+                                  ))}
+                                </TableBody>
+                              </Table>
+                            )}
+                          </CardContent>
+                      </Card>
+                    </Box>
                 </Grid>
                 <Grid item xs={12} md={7}>
                     <Card>
@@ -134,7 +160,7 @@ const Dashboard: React.FC = () => {
                             Expenses
                         </Typography>
                         {isLoadingExpenses ? (
-                            <Typography>Loading expenses...</Typography>
+                            <LoadingSpinner />
                         ) : isErrorExpenses ? (
                             <Typography color="error">{(expensesError as Error).message || 'Failed to fetch expenses'}</Typography>
                         ) : (
